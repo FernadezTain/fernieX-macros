d77cec76ec5179eb51007db169111ff7768576ca
FernadezTain <fernadezofficial489@gmail.com>
2026-02-14 18:34:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -796,27 +796,8 @@ function createMacro() {
         return;
     }
 
-    // Функция транслитерации кириллицы в латиницу
-    function transliterate(text) {
-        const map = {
-            'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
-            'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
-            'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
-            'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch',
-            'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
-            'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo',
-            'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
-            'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
-            'Ф': 'F', 'Х': 'H', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Sch',
-            'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya',
-            ' ': '_', '.': '', ',': '', '!': '', '?': '', ':': '', ';': ''
-        };
-        
-        return text.split('').map(char => map[char] || char).join('');
-    }
-
     // Формируем компактные данные макроса
-    const parts = [transliterate(state.triggerValue)];
+    const parts = [state.triggerValue];  // БЕЗ транслитерации!
     
     state.actions.forEach(action => {
         if (action.type === 'get_top_position') {
@@ -836,8 +817,7 @@ function createMacro() {
                 .replace(/\{topresultID\}/g, 'TOPID')
                 .replace(/\{me\}/g, 'ME');
             
-            // Транслитерируем текст
-            messageText = transliterate(messageText);
+            // БЕЗ транслитерации! Оставляем текст как есть
             
             parts.push(`m-${messageText}`);
         } else if (action.type === 'send_photo') {
